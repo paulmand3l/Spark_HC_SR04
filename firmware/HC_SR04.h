06e2baf3c85fdb58310863af567ce83586b0d124
Paul Mandel <paul.mand3l@gmail.com>
2015-04-27 16:04:55 -0700

    Adding configurable timeout

--- firmware/HC_SR04.h
+++ firmware/HC_SR04.h
@@ -8,6 +8,7 @@ class HC_SR04
 public: 
   HC_SR04(int trigPin, int echoPin);
   HC_SR04(int trigPin, int echoPin, double minCM, double maxCM); 
+  HC_SR04(int trigPin, int echoPin, double minCM, double maxCM, long timeout);
   double getDistanceCM();  
   double getDistanceInch();
   
@@ -16,6 +17,7 @@ private:
   int _trigPin;
   double _minCM;
   double _maxCM;
+  long _timeout;
   
   void sendTriggerPulse(int pin);
   void waitForEcho(int pin, int value, long timeout);
